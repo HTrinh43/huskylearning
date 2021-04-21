dcba247d6e788ef914e009e6b13468ff68cc65c0
Alex_mac <huy.trinh43@icloud.com>
2021-04-21 04:59:44 -0700

    add course

---
+++ app.py
@@ -0,0 +1,34 @@
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+def create_app():
+    #Creating new App
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "Simple Secret Key"
+
+    # # Adding database to the application
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # Adding routes
+    from website import routes
+    app.register_blueprint(routes.routes, url_prefix="/")
+
+    # Create Database
+    create_database(app)
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Database Created")
+
+app = create_app()
+if __name__=='__main__':
+    app.run(debug=True)
